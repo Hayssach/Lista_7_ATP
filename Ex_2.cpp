d99131222de51abba0107dc7b2ca24137dcb767f
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 14:07:23 -0300

    Ex_2.cpp

--- Ex_2.cpp
+++ Ex_2.cpp
@@ -2,27 +2,25 @@
 #include <stdlib.h>
 
 int main(){
-	 int A[5], B[5];
+	 int I[5], J[5];
 
-    // Leitura dos valores para o vetor A
-    printf("Digite 5 valores para o vetor A:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("A[%d]: ", i);
-        scanf("%d", &A[i]);
+    printf("Digite 5 valores para o vetor I:\n");
+    for (int j = 0; j < 5; j++) {
+        printf("I[%d]: ", j);
+        scanf("%d", &I[j]);
     }
 
-    // Leitura dos valores para o vetor B
-    printf("Digite 5 valores para o vetor B:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("B[%d]: ", i);
-        scanf("%d", &B[i]);
+    printf("Digite 5 valores para o vetor J:\n");
+    for (int j = 0; j < 5; j++) {
+        printf("J[%d]: ", j);
+        scanf("%d", &J[j]);
     }
 
-    // Impressão intercalada dos valores
+    // ImpressÃ£o intercalada dos valores
     printf("\nValores intercalados:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", A[i]);
-        printf("%d ", B[i]);
+    for (int j = 0; j < 5; j++) {
+        printf("%d ", I[j]);
+        printf("%d ", J[j]);
     }
 
     printf("\n");
